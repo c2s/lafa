c81f5f9eec1f1e19295b43fa1235e4b0cc596acd
mofei <linux.root.users@gmail.com>
2019-01-27 00:43:40 +0800

    update

---
+++ app/Models/Hh.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * LaFa - A Laravel Fast Development Framework For Web Artisans
+ *
+ * @author   mofei <root@mofei.org>
+ * @link     https://github.com/imofei/lafa
+ * @date     2018-02-01 23:18
+ */
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Storage;
+use App\Events\BehaviorLogEvent;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+/**
+ * 友情链接模型
+ *
+ * Class Link
+ * @package App\Models
+ */
+class Hh extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'id',
+        'name',
+        'description',
+        'url',
+        'order',
+        'rating',
+        'image',
+        'target',
+        'rel',
+        'status'
+    ];
+
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    public $dispatchesEvents  = [
+        'saved' => BehaviorLogEvent::class,
+    ];
+
+    public function titleName(){
+        return 'name';
+    }
+}
