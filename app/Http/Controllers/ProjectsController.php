396a610f1527dd99c1530d6ca934fd81e473055b
mofei <linux.root.users@gmail.com>
2019-10-16 11:24:13 +0800

    更新项目注释

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -19,34 +19,70 @@ class ProjectsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
+    /**
+     * 首页
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
 	public function index()
 	{
 		$projects = Project::paginate();
 		return view('projects.index', compact('projects'));
 	}
 
+    /**
+     * 详情
+     * @param Project $project
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(Project $project)
     {
         return view('projects.show', compact('project'));
     }
 
+    /**
+     * 创建
+     *
+     * @param Project $project
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
 	public function create(Project $project)
 	{
 		return view('projects.create_and_edit', compact('project'));
 	}
 
+    /**
+     * 保存
+     *
+     * @param ProjectRequest $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
 	public function store(ProjectRequest $request)
 	{
 		$project = Project::create($request->all());
 		return redirect()->route('projects.show', $project->id)->with('message', 'Created successfully.');
 	}
 
+    /**
+     * 编辑
+     *
+     * @param Project $project
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
 	public function edit(Project $project)
 	{
         $this->authorize('update', $project);
 		return view('projects.create_and_edit', compact('project'));
 	}
 
+    /**
+     * 更新
+     *
+     * @param ProjectRequest $request
+     * @param Project $project
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Illuminate\Auth\Access\AuthorizationException
+     */
 	public function update(ProjectRequest $request, Project $project)
 	{
 		$this->authorize('update', $project);
